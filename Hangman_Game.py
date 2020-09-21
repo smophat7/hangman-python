e423b64e17df74cb7df63cbbf721c66219e8af6c
Spencer Moffat <spencer.g.moffat@gmail.com>
2020-09-20 20:33:19 -0600

    Fixed some bugs and commented out incomplete features

--- Hangman_Game.py
+++ Hangman_Game.py
@@ -1,30 +1,20 @@
-#Created 6/3/2020
-#First mini project for Python. Everything else was me just fooling around figuring out how things work.
-
-#This is a basic hangman game. It would be good practice to add a few more features, such as:
-    #Helpful hints (you only get 2, and they are things like "There are no more vowels" or "first 8 letters of the
-    #   alphabet" or something like that. Maybe use both to just request a free letter)
-    #Create Single and Multiplayer options for a random word or a user entered one (maybe give suggestions)
-
-    #Have multiple words instead of just one (create sentences or phrases, but that'd be a whole new website source)
-
-#AMBITIOUS: If I really want to challenge myself, I could create a website or app to play with visuals and crap
+# Initially created: 6/3/2020
 
 import random
 import urllib.request
 
-#Get list of random words from this website, will filter words later once user picks settings
+# Get list of random words from this website, filter words later user picks settings
 with urllib.request.urlopen("http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain") as response:
     file = response.read().decode()
-allwords = file.splitlines()
+allWords = file.splitlines()
 
-#Set maximum maxWordLength (don't know if this website ever change (probably not but it's the principle))
+# Set maximum maxWordLength (just in case the website changes at all)
 lengthLimit = 0
-for word in allwords:
+for word in allWords:
     if len(word) > lengthLimit:
         lengthLimit = len(word)
 
-#initalizing variables (don't think this is a thing in Python, but it is conventional in C++)
+# Variables
 name = ""
 livesDesired = 0
 numLives = 6
@@ -47,11 +37,12 @@ def updateDisplay(letter):
         displayAnswer[index] = letter
         i += 1
 
-#The game itself
+
+# The game itself
 print("Welcome to Hangman!")
 while continuePlaying == True:
 
-    #Changing the Difficulty Settings
+    # Changing the Difficulty Settings
     name = input("What would you like to name your hanging man? ")
     while ifDifficultySet == False:
         difficulty = input("What difficulty settings would you like to use? (Enter \"standard\" or \"custom\") ")
@@ -81,7 +72,7 @@ while continuePlaying == True:
                     break
                 else:
                     print("Invalid entry. Please try again.")
-                    if minWordLength > lengthLimit:
+                    if minWordLength == int and minWordLength > lengthLimit:
                         print("(The longest word available is only " + str(lengthLimit) + " letters long.)\n")
             while ifCorrectEntry == False:
                 maxWordLength = input("What would you like the maximum word length to be? ")
@@ -92,56 +83,77 @@ while continuePlaying == True:
                     break
                 else:
                     print("Invalid entry. Please try again.")
-                    if maxWordLength > lengthLimit:
+                    if type(maxWordLength) == int and maxWordLength > lengthLimit:
                         print("(The longest word available is only " + str(lengthLimit) + " letters long.)")
             break
         else:
             print("Invalid entry. Please try again.")
 
-    #Generate answer word based off of these requirements
-    words = [word for word in allwords if (word.islower() and len(word) >= minWordLength and len(word) <= maxWordLength)]
+    # Generate the answer word based off of these requirements
+    words = [word for word in allWords if (word.islower() and len(word) >= minWordLength and len(word) <= maxWordLength)]
     answer = words[random.randrange(len(words))]
     displayAnswer = []
     for i in answer:
         displayAnswer.append("_")
     
-    #Final instructions before gameplay
+    # Final instructions before gameplay
     print("\nPerfect! Guess each letter in the word. Miss " + str(numLives) + " time(s) and you kill " + name + ".")
-    print("You get ??????????? hint(s). Type \"hint\" instead of a guess to request one.")           #finish this one up
-    print("Also, enter \"choices\" to see what letters you have already guessed and what is still available")
+    #print("You get ??????????? hint(s). Type \"hint\" instead of a guess to request one.")                          # feature to add
+    print("Also, enter \"choices\" to see what letters you have already guessed and what is still available.")
+    print("If you ever need to see this menu again, enter \"help\" and you will in fact be helped.")
     print("This word has", len(answer), "letters. Good luck!")
 
-    #The actual game begins here
+    # The actual game begins here
     while numLives > 0:
         print("\n\n" + " ".join(displayAnswer) +"\n")
 
-        #If the round has been completely solved
+        # If the round has been completely solved
         if not "_" in displayAnswer:
             print("Congratulations, you saved " + name + "\nGo celebrate with his family :)")
             break
 
-        #Getting user's letter guess / input
+        # Getting user's letter guess / input
         ifNewGuess = False
         while ifNewGuess == False:
-            guess = input("Enter your guess: ")
+            guess = input("Enter your guess (or enter \"help\"): ")
 
-            #If the input is a "hint" or "show guesses" type request
-            if guess == "hint":
-                print("Will display a hint of some sort")                               #DELETE ME and finish this
-                #formulate and display a hint
+            # If the input isn't in the alphabet 
+            if not guess.isalpha():
+                print("That doesn't make sense and you know it. Enter someting valid next time.")
+                print("Let's try this again, shall we?")
+
+            # If the input is to see what they can still guess
             elif guess == "choices":
-                print("You've already guessed:\n    " + str(guessedLetters))
-                print("You can still guess:\n    " + str(unguessedLetters))
+                print("you've already guessed:\n    " + str(guessedLetters))
+                print("you can still guess:\n    " + str(unguessedLetters))
             
-            #Dealing with an actual letter guess
+            #elif guess == "hint":                                            # feature to add
+            #    print("Will display a hint of some sort")
+            #    # formulate and display a hint
+
+            # If the user is asking for help
+            elif guess == "help":
+                print("\n Guess each letter in the word. If you miss " + str(numLives) + " time(s), you kill " + name + ".")
+                #print("You get ??????????? hint(s). Type \"hint\" instead of a guess to request one.")                          # feature to add
+                print("Also, enter \"choices\" to see what letters you have already guessed and what is still available.")
+                print("If you ever need to see this menu again, enter \"help\" and you will in fact be helped.")
+                print("This word has", len(answer), "letters. You got this!")
+
+            # If the input is more than 1 character
+            elif len(guess) > 1:
+                print("I'm sure you know that just isn't allowed.")
+                print("Please try again (and a little harder this time?")
+
+            # Dealing with an actual letter guess
             elif guess in guessedLetters:
                 print("You have already guessed this letter. Try something else.")
+
             else:
                 ifNewGuess = True
                 guessedLetters.append(guess)
                 unguessedLetters.remove(guess)
 
-        #If the guess is correct
+        # If the guess is correct
         if answer.find(guess) != -1:
             if answer.count(guess) == 1:
                 print("\nCorrect! There is 1 " + guess.capitalize() + " in this word.")
@@ -149,7 +161,7 @@ while continuePlaying == True:
                 print("\nCorrect! There are " + str(answer.count(guess)) + " " + guess.capitalize() + "s in this word.")
             updateDisplay(guess)
 
-        #If the guess is incorrect
+        # If the guess is incorrect
         else:
             numLives -= 1
             if numLives == 0:
@@ -160,7 +172,7 @@ while continuePlaying == True:
             print("\nIncorrect! " + name + " is now one step closer to death...")
             print("Number of incorrect guesses remaining:", numLives)
 
-    #Determine if playing another round
+    # Determine if playing another round
     print("\nThanks for playing!\n")
     playAgain = input("Would you like to play again? (Enter \"yes\" or \"no\") ")
     if playAgain == "yes":
@@ -179,5 +191,5 @@ while continuePlaying == True:
         print("Well fine, then. Hope you had a good time.")
         continuePlaying = False
 
-#Loop exited, game over
+# Loop exited, game over
 print("\nThanks for playing!\n")
